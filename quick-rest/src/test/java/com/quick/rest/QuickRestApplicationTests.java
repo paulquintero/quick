917e6fb9139e086811c81711dda952f65e0a1c86
Paul Quintero <paul.alonzoqr@gmail.com>
2022-10-18 20:40:49 -0600

    base templates

--- quick-rest/src/test/java/com/quick/rest/QuickRestApplicationTests.java
+++ quick-rest/src/test/java/com/quick/rest/QuickRestApplicationTests.java
@@ -1,11 +1,17 @@
 package com.quick.rest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QuickRestApplicationTests {
 
+	@BeforeEach
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
 	@Test
 	void contextLoads() {
 	}
